4d43cebd648dfb548dc687785227ebacd35c236c
Leonardo Osugi <leoosugi96@gmail.com>
2022-08-14 16:28:46 -0300

    Menu With Buggy Check

--- script.js
+++ script.js
@@ -1,30 +1,69 @@
 
 function opcao(seletor){
-    const cardapio = document.querySelector('.lateral-menu .green');
+    const cardapio = document.querySelector('.prato .green');
+
 
     if(cardapio !== null){
+        const confirm = document.querySelector('.prato .green .chekando ');
+        confirm.classList.add("none");
         cardapio.classList.remove("green");
     }
 
-    seletor.classList.add("green");
-}
+    const caixaCheckao = document.querySelector(seletor);
+    caixaCheckao.classList.add("green");
 
-function opcao2(seletor){
-    const cardapio2 = document.querySelector('.lateral-menu2 .green');
-
-    if(cardapio2 !== null){
-        cardapio2.classList.remove("green");
+    const procura = document.querySelector(`${seletor} .chekando`);
+    if(procura !== null){
+        procura.classList.remove("none");
     }
 
-    seletor.classList.add("green");
+
+    return true;
 }
 
-function opcao3(seletor){
-    const cardapio3 = document.querySelector('.lateral-menu3 .green');
 
-    if(cardapio3 !== null){
-        cardapio3.classList.remove("green");
+function opcao2(seletor2){
+    const cardapio = document.querySelector('.bebida .green');
+
+    console.log(cardapio);
+    if(cardapio !== null){
+        const confirm = document.querySelector('.bebida .green .chekando');
+        confirm.classList.add("none");
+        cardapio.classList.remove("green");
     }
 
-    seletor.classList.add("green");
+    const caixaCheckao = document.querySelector(seletor2);
+    caixaCheckao.classList.add('green');
+
+    const procura = document.querySelector(`${seletor2} .chekando`);
+    if(procura !== null){
+        procura.classList.remove("none");
+    }
+
+
+    return true;
+}
+
+function opcao3(seletor3){
+    const cardapio = document.querySelector('.sobreamesa .green');
+
+    console.log(cardapio);
+    if(cardapio !== null){
+        const confirm = document.querySelector('.sobreamesa .green .chekando');
+        confirm.classList.add("none");
+        cardapio.classList.remove("green");
+    }
+
+    const caixaCheckao = document.querySelector(seletor3);
+    caixaCheckao.classList.add('green');
+
+    const procura = document.querySelector(`${seletor3} .chekando`);
+    if(procura !== null){
+        procura.classList.remove("none");
+    }
+
+
+    return true;
+
+    return true;
 }
