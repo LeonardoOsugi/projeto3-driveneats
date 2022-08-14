39ceef33a53e29efd19b19db5ff3eccfc6d8b6bc
Leonardo Osugi <leoosugi96@gmail.com>
2022-08-13 22:32:44 -0300

    Almost ready

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
+
 function opcao(seletor){
-    const cardapio = document.querySelector(".content .green");
+    const cardapio = document.querySelector('.lateral-menu .green');
 
     if(cardapio !== null){
         cardapio.classList.remove("green");
+    }
+
+    seletor.classList.add("green");
+}
+
+function opcao2(seletor){
+    const cardapio2 = document.querySelector('.lateral-menu2 .green');
+
+    if(cardapio2 !== null){
+        cardapio2.classList.remove("green");
+    }
+
+    seletor.classList.add("green");
+}
+
+function opcao3(seletor){
+    const cardapio3 = document.querySelector('.lateral-menu3 .green');
+
+    if(cardapio3 !== null){
+        cardapio3.classList.remove("green");
     }
 
     seletor.classList.add("green");
