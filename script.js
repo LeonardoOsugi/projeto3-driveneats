2ba22a5077de8f696f9891818817efe463d6aafb
Leonardo Osugi <leoosugi96@gmail.com>
2022-08-15 17:58:32 -0300

    Versão Final da Entrega

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let seletor_1 = undefined;
+let seletor_2 = undefined;
+let seletor_3 = undefined;
+let nome_1;
+let nome_2;
+let nome_3;
+let valorTotal;
+
+
+const tacada =  document.querySelector('.ultima-parada .pedido');
+tacada.disabled = true;
+
+function enviar(){
+    if(seletor_1 !== undefined && seletor_2 !== undefined && seletor_3 !== undefined){
+        tacada.disabled = false;
+        tacada.innerHTML = "Fechar pedido";
+        tacada.classList.add('background-botao');
+    } 
+}
 
 function opcao(seletor){
     const cardapio = document.querySelector('.prato .green');
@@ -12,20 +31,27 @@ function opcao(seletor){
     const caixaCheckao = document.querySelector(seletor);
     caixaCheckao.classList.add("green");
 
+    
     const procura = document.querySelector(`${seletor} .chekando`);
     if(procura !== null){
         procura.classList.remove("none");
     }
 
+    
+    const moneyPrato = document.querySelector(`${seletor} .valor-prato`);
+    seletor_1 = moneyPrato.innerHTML;
 
-    return true;
+    const nomePrato = document.querySelector(`${seletor} .nome-prato`);
+    nome_1 = nomePrato.innerHTML;
+
+    enviar();
 }
 
 
 function opcao2(seletor2){
+    seletor_2 = seletor2;
     const cardapio = document.querySelector('.bebida .green');
 
-    console.log(cardapio);
     if(cardapio !== null){
         const confirm = document.querySelector('.bebida .green .chekando');
         confirm.classList.add("none");
@@ -40,14 +66,19 @@ function opcao2(seletor2){
         procura.classList.remove("none");
     }
 
+    const moneyBerita = document.querySelector(`${seletor2} .valor-berita`);
+    seletor_2 = moneyBerita.innerHTML;
 
-    return true;
+    const nomeBerita = document.querySelector(`${seletor2} .nome-bebida`);
+    nome_2 = nomeBerita.innerHTML;
+
+    enviar();
 }
 
 function opcao3(seletor3){
+    seletor_3 = seletor3;
     const cardapio = document.querySelector('.sobreamesa .green');
 
-    console.log(cardapio);
     if(cardapio !== null){
         const confirm = document.querySelector('.sobreamesa .green .chekando');
         confirm.classList.add("none");
@@ -61,9 +92,27 @@ function opcao3(seletor3){
     if(procura !== null){
         procura.classList.remove("none");
     }
+    
+    const moneySobremesa = document.querySelector(`${seletor3} .valor-sobremesa`);
+    seletor_3 = moneySobremesa.innerHTML;
 
+    const nomeSobremesa = document.querySelector(`${seletor3} .nome-sobremesa`);
+    nome_3 = nomeSobremesa.innerHTML;
 
-    return true;
+    enviar();
 
-    return true;
+}
+
+function enviarZap(){
+    valorTotal = (Number(seletor_1.replace(',', '.'))) + (Number(seletor_2.replace(',', '.'))) + (Number(seletor_3.replace(',', '.')));
+    const variavel = `Olá, gostaria de fazer o pedido:
+    - Prato: ${nome_1}
+    - Bebida: ${nome_2}
+    - Sobremesa: ${nome_3}
+    Total: R$ ${(valorTotal.toFixed(2))}`
+    
+
+        let zipzop = encodeURIComponent(variavel);
+
+        window.open(`https://wa.me/5541998096214?text=${zipzop}`);
 }
